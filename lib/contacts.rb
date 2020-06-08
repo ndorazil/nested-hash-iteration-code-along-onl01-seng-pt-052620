6226350000dbe224aeb2c5084d806dbf88e84b9e
Nathan Dorazil <nathand1915@gmail.com>
2020-06-08 03:46:24 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,9 +15,10 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  if person == "Freddy Mercury"
-    contacts.each do |person, contact_details_hash|
-      binding.pry
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contacts_details_hash.each do |attribute, data|
+        binding.pry
     end
   end
 end
